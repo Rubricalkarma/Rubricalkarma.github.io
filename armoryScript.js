55f0b399d6fcd860e79639165e3b4c9135954e57
Rubricalkarma <37121035+Rubricalkarma@users.noreply.github.com>
2018-04-04 01:55:18 -0500

    1.3.2
    Recalls recent searches

--- armoryScript.js
+++ armoryScript.js
@@ -1,34 +1,70 @@
 
 
+var apiKey="dfp2dz9s5mjnpsxyk3zatz9zc8mpmmq8";
 var apiProgression = "https://us.api.battle.net/wow/character/zuljin/magepie?fields=progression&locale=en_US&apikey=dfp2dz9s5mjnpsxyk3zatz9zc8mpmmq8";
+var apiRaceInfo = "https://us.api.battle.net/wow/data/character/races?locale=en_US&apikey=dfp2dz9s5mjnpsxyk3zatz9zc8mpmmq8";
+var apiClassInfo = "https://us.api.battle.net/wow/data/character/classes?locale=en_US&apikey=dfp2dz9s5mjnpsxyk3zatz9zc8mpmmq8";
+var apiRealm = "https://us.api.battle.net/wow/realm/status?locale=en_US&apikey=dfp2dz9s5mjnpsxyk3zatz9zc8mpmmq8";
+
 var raidInfoObject;
 var realmList=[];
 
+var recentCharacters=[];
+recentCharacters=JSON.parse(localStorage.getItem('recentCharacters'));
+
 function getRealms(){
-	var realmInfoObject;
-	var apiRealm = "https://us.api.battle.net/wow/realm/status?locale=en_US&apikey=dfp2dz9s5mjnpsxyk3zatz9zc8mpmmq8";
 
-	$.ajax({
-		url: apiRealm,
-		async: false,
-		dataType: 'json',
-		success: function (json) {
-			realmInfoObject = json;
-		}
-	});
-
+	//gets realms from local storage
+	var realmInfoObject = JSON.parse(localStorage.getItem('realmInfoObject'));
+	//if the realms JSON is empty, calls api and stores realminfo
+	if(realmInfoObject===null){
+		$.ajax({
+			url: apiRealm,
+			async: false,
+			dataType: 'json',
+			success: function (json) {
+				realmInfoObject = json;
+				console.log("Realm info loaded from API");
+				localStorage.setItem('realmInfoObject', JSON.stringify(realmInfoObject));
+			}
+		});
+	}else{
+		//if JSON is not empty, continues as normal
+		console.log("Realm info loaded from Local Storage");
+	}
+	//adds all realms to array
 	for(let i=0;i<realmInfoObject.realms.length;i++){
 		realmList.push(realmInfoObject.realms[i].name);
 	}
-	
-	$( function() {
 
+	$( function() {
+		//sets autocomplete for serverSearch with realmList array
 		$( "#serverSearch" ).autocomplete({
+			minLength: 0,
 			source: realmList,
+			position: {my: "left+0 top+0",},
+		})
+
+		.focus(function() {
+			$(this).autocomplete('search', $(this).val())
 		});
-	} );
+	});
+
+	$( function() {
+		//sets autocomplete for serverSearch with realmList array
+		$( "#characterSearch" ).autocomplete({
+			minLength: 0,
+			source: recentCharacters,
+		})
+
+		.focus(function() {
+			$(this).autocomplete('search', $(this).val())
+		});
+	});
+
 
 }
+
 
 
 function getProgressionInfo(){
@@ -36,74 +72,84 @@ function getProgressionInfo(){
 	var character = document.getElementById("characterSearch").value;
 	var apiProgression = 'https://us.api.battle.net/wow/character/'+server+'/'+character+'?fields=progression&locale=en_US&apikey=dfp2dz9s5mjnpsxyk3zatz9zc8mpmmq8';
 	var apiItems = 'https://us.api.battle.net/wow/character/'+server+'/'+character+'?fields=items&locale=en_US&apikey=dfp2dz9s5mjnpsxyk3zatz9zc8mpmmq8';
+	recentCharacters.push(character);
+	localStorage.setItem('recentCharacters', JSON.stringify(recentCharacters));
+	var itemInfoObject;
 
-
-
-	$.getJSON(apiItems, function(data){
-
-		var color = "gray";
-		var itemLevel = data.items.averageItemLevel;
-
-		if(itemLevel>960){
-			color="orange";
+	$.ajax({
+		url: apiItems,
+		async: false,
+		dataType: 'json',
+		success: function (json) {
+			itemInfoObject = json;
 		}
-		else if(itemLevel>940){
-			color="purple";
-		}
-		else if(itemLevel>920){
-			color="blue";
-		}
-		else if(itemLevel>900){
-			color="green";
-		}
-		else if(itemLevel>880){
-			color="white";
-		}
-		var profileImage = data.thumbnail.replace("avatar.jpg", "profilemain.jpg");
+	});
 
-		document.getElementById("itemLevel").style = "color: "+color;
-		document.getElementById("itemLevel").innerHTML = itemLevel;
-		document.getElementById("icon").src = "http://render-us.worldofwarcraft.com/character/"+profileImage;
-		
+	var color = "gray";
+	var itemLevel = itemInfoObject.items.averageItemLevel;
+
+	if(itemLevel>960){
+		color="orange";
+	}
+	else if(itemLevel>940){
+		color="purple";
+	}
+	else if(itemLevel>920){
+		color="blue";
+	}
+	else if(itemLevel>900){
+		color="green";
+	}
+	else if(itemLevel>880){
+		color="white";
+	}
+	var profileImage = itemInfoObject.thumbnail.replace("avatar.jpg", "profilemain.jpg");
+
+	document.getElementById("itemLevel").style = "color: "+color;
+	document.getElementById("itemLevel").innerHTML = itemLevel;
+	document.getElementById("icon").src = "http://render-us.worldofwarcraft.com/character/"+profileImage;
+
+	$.ajax({
+		url: apiProgression,
+		async: false,
+		dataType: 'json',
+		success: function (json) {
+			progressionInfoObject = json;
+		}
 	});
 
 
-	$.getJSON(apiProgression, function(data) {
-
-		var raidIndex = 0;
-		var bossIndex=0;
-		var raidName = document.getElementById("raidSelect").value;
-		var bossName = document.getElementById("bossSelect").value;
+	var raidIndex = 0;
+	var bossIndex=0;
+	var raidName = document.getElementById("raidSelect").value;
+	var bossName = document.getElementById("bossSelect").value;
 
 
-		for(var j=0;j<data.progression.raids.length;j++){
-			if(data.progression.raids[j].name === raidName){
-				raidIndex=j;
-				break;
-			}
+	for(var j=0;j<progressionInfoObject.progression.raids.length;j++){
+		if(progressionInfoObject.progression.raids[j].name === raidName){
+			raidIndex=j;
+			break;
 		}
+	}
 
-		for(var k=0;k<data.progression.raids[raidIndex].bosses.length;k++){
-			if(data.progression.raids[raidIndex].bosses[k].name===bossName){
-				bossIndex=k;
-				break;
-			}
+	for(var k=0;k<progressionInfoObject.progression.raids[raidIndex].bosses.length;k++){
+		if(progressionInfoObject.progression.raids[raidIndex].bosses[k].name===bossName){
+			bossIndex=k;
+			break;
 		}
+	}
 
-		document.getElementById("name").style.color=getClassColor(data.class);
-		document.getElementById("name").innerHTML = data.name+" - "+data.realm;
-		document.getElementById("characterInfo").style.color=getClassColor(data.class);
-		document.getElementById("characterInfo").innerHTML = "level "+data.level+" "+getRace(data.race)+" "+getClass(data.class);
-		document.getElementById("kills").innerHTML = 
-		"<div>"+data.progression.raids[raidIndex].name+" - "+data.progression.raids[raidIndex].bosses[bossIndex].name+"</div><br/>"+
-		"<div style='color: green'>LFR: "+data.progression.raids[raidIndex].bosses[bossIndex].lfrKills+"</div><br/>"+
-		"<div style='color: orange'>Normal: "+data.progression.raids[raidIndex].bosses[bossIndex].normalKills+"</div><br/>"+
-		"<div style='color: yellow'>Heroic: "+data.progression.raids[raidIndex].bosses[bossIndex].heroicKills+"</div><br/>"+
-		"<div style='color: red'>Mythic: "+data.progression.raids[raidIndex].bosses[bossIndex].mythicKills+"</div><br/>"
-		
+	document.getElementById("name").style.color=getClassColor(progressionInfoObject.class);
+	document.getElementById("name").innerHTML = progressionInfoObject.name+" - "+progressionInfoObject.realm;
+	document.getElementById("characterInfo").style.color=getClassColor(progressionInfoObject.class);
+	document.getElementById("characterInfo").innerHTML = "level "+progressionInfoObject.level+" "+getRace(progressionInfoObject.race)+" "+getClass(progressionInfoObject.class);
+	document.getElementById("kills").innerHTML = 
+	"<div>"+progressionInfoObject.progression.raids[raidIndex].name+" - "+progressionInfoObject.progression.raids[raidIndex].bosses[bossIndex].name+"</div><br/>"+
+	"<div style='color: green'>LFR: "+progressionInfoObject.progression.raids[raidIndex].bosses[bossIndex].lfrKills+"</div><br/>"+
+	"<div style='color: orange'>Normal: "+progressionInfoObject.progression.raids[raidIndex].bosses[bossIndex].normalKills+"</div><br/>"+
+	"<div style='color: yellow'>Heroic: "+progressionInfoObject.progression.raids[raidIndex].bosses[bossIndex].heroicKills+"</div><br/>"+
+	"<div style='color: red'>Mythic: "+progressionInfoObject.progression.raids[raidIndex].bosses[bossIndex].mythicKills+"</div><br/>"
 
-
-	});
 
 
 }	
@@ -142,7 +188,7 @@ function getClassColor(number){
 
 function getClass(number){
 
-	var apiClassInfo = "https://us.api.battle.net/wow/data/character/classes?locale=en_US&apikey=dfp2dz9s5mjnpsxyk3zatz9zc8mpmmq8";
+
 
 	var classObject = null;
 
@@ -163,7 +209,7 @@ function getClass(number){
 }
 
 function getRace(number){
-	var apiRaceInfo = "https://us.api.battle.net/wow/data/character/races?locale=en_US&apikey=dfp2dz9s5mjnpsxyk3zatz9zc8mpmmq8";
+
 
 	var raceObject = null;
 
